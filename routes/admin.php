573a52066db3ededfb448541971f9275d4768b07
Nam <nam79079@st.vimaru.edu.vn>
2022-02-23 09:26:13 +0700

    n

--- routes/admin.php
+++ routes/admin.php
@@ -7,5 +7,5 @@ use Illuminate\Support\Facades\Route;
 Route::match(['get', 'post'], '/', 'Admin\Auth\LoginController@login')->name('admin.login');
 Route::middleware('auth:admin')->group(function (){
     Route::get('/home', 'Admin\HomeController@index')->name('dashboard');
-    Route::get('/san-pham','Admin\ProductController@index');
+    Route::get('/product','Admin\ProductController@index');
 });
